6e6ade224c1a282164832b6d4dd747511a5cdec7
B0929054 <100269353+B0929054@users.noreply.github.com>
2022-04-04 19:16:37 +0800

    Update qz3.js

--- qz3/qz3.js
+++ qz3/qz3.js
@@ -1,4 +1,4 @@
-document.addEventListener("keydown",logkey);   //使用keydown輸出會延遲一步，使用keyup卻沒有，為什麼?
+document.addEventListener("key",logkey);   //使用keydown輸出會延遲一步，使用keyup卻沒有，為什麼?
 function logkey() {
 let x=document.getElementById("word").value; 
 document.getElementById("constant").innerHTML = x;
