4d6ed54ce2fc22c3978dea70196779fa90c19800
B0929054 <100269353+B0929054@users.noreply.github.com>
2022-04-04 19:11:30 +0800

    Add files via upload

---
+++ qz3/qz3.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown",logkey);
+//使用keydown輸出會延遲一步，使用keyup卻沒有，為什麼
+function logkey() {
+let x=document.getElementById("word").value; 
+document.getElementById("constant").innerHTML = x;
+}
