dc42271358c30604cb338b52112962388d89b44a
B0929054 <100269353+B0929054@users.noreply.github.com>
2022-04-27 18:27:31 +0800

    Update qz7.js

--- qz7/qz7.js
+++ qz7/qz7.js
@@ -33,11 +33,13 @@ rain: 0.0
 }
 ];
 var currentTime=0;
-for (let i=0; i<2; i++) {
+for (let i=0; i<5; i++) {
 var year=Math.floor(data[i].recTime/100000000);
 var month=Math.floor((data[i].recTime/1000000)%100);
 var day=Math.floor((data[i].recTime/10000)%100);
+var hour=Math.floor((data[i].recTime/100)%100);
+var minute=Math.floor((data[i].recTime)%100);
 $("#contain").append('<div><h2>'+data[i].stationName+'('+data[i].stationNo+')'+'</h2></div>')
-$("#contain").append('<div><h3>'+year+'年'+month+'月'+day+'號'+'</h3></div>')
+$("#contain").append('<div><h3>'+year+'年'+month+'月'+day+'號'+hour+'時'+minute+'分'+'</h3></div>')
 $("#contain").append('<div><h3>'+'即時雨量'+data[i].rain+'</h3></div>')
 }
